15699ca881caabd66fe37f6ed42ede9a04d9a13d
root <root@debian8>
2017-11-11 09:57:25 -0500

    add main.c, loopfunc.c

---
+++ loopfunc.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void loopfunc(int c){
+	int i;
+	for (i=1;i<=c;i++){
+		printf("i=%d\t",i);
+	}
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(char* argv, int argc){
+	int loopnum=10000;
+	loopfunc(loopnum);
+	printf("Hello, project1.\n");
+	return 0;
+}
